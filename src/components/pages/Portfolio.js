5f9b66811dd83bd42fbb03b0fb4ef69e0e6f3ab0
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-03-08 11:10:06 -0800

    added description for the projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,19 +4,19 @@ const projects = [
   {
     title: "Project 1",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis semper velit.",
+      "Weather DashBoard Application can show you the current and the next 5 days forecast weather for multiple cities. By input your desire city in the search box, you can see the weather pops up after you hit the search button. The dashboard will show you the Date, Temp, Wind, and Humidity of the city that you choose.",
     link: "https://linhklatt.github.io/weather-dashboard/",
   },
   {
     title: "Project 2",
     description:
-      "Mauris facilisis odio eget nunc varius, vel sollicitudin lectus vehicula.",
+      "A website that displays a map with hidden gems placed using markers. The user can select the marker and view and image, the address, and the 3 most recent reviews for the location. User wil also be able to submit their own hidden gems on the submit page.",
     link: "https://eddygoto.github.io/Project-1-Travel-Site/",
   },
   {
     title: "Project 3",
     description:
-      "Nullam tincidunt arcu vitae nisi dapibus venenatis. Sed commodo tincidunt urna, ut hendrerit est euismod vel.",
+      "Introducing an epic browser-based fighting game that immerses players in a fantasy world where they can choose to be a powerful Warrior, a skilled Archer, or a masterful Mage. Each class has its own unique characteristics, strengths, and weaknesses, allowing players to customize their gameplay experience and compete against other characters in real-time battles. With stunning graphics, smooth animations, and challenging AI opponents, this game will keep players coming back for more action-packed adventure.",
     link: "https://dbfighterz.herokuapp.com/",
   },
 ];
