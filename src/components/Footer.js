7afe6d6569f875fceaf0f1a74e64dd8bd414ac32
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-03-07 21:25:49 -0800

    added Footer and Navidation

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+export default function Footer() {
+  return (
+    <footer className="text-center text-white fixed-bottom ">
+      <div className="container p-4 pb-0">
+        <section className="mb-4">
+          <a
+            className="btn btn-primary btn-floating m-1"
+            href="https://www.linkedin.com/in/linh-klatt-0bb889b6/"
+            role="button"
+          >
+            <FaLinkedin />
+          </a>
+          <a
+            className="btn btn-dark btn-floating m-1"
+            href="https://github.com/linhklatt"
+            role="button"
+          >
+            <FaGithub />
+          </a>
+          <a
+            className="btn btn-dark btn-floating m-1"
+            href="https://twitter.com/home"
+            role="button"
+          >
+            <FaTwitter />
+          </a>
+        </section>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+export default function Navigation(props) {
+  return (
+    <Navbar collapseOnSelect expand="sm" className="row p-3 text-light">
+      <Container>
+        <h2>Linh KLatt</h2>
+        <Navbar.Brand href="#home"></Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          id="custom-nav-button"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link
+              href="#aboutme"
+              className="text-light custom-nav"
+              onClick={() => props.handlePageChange("AboutMe")}
+            >
+              About Me
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className="text-light custom-nav"
+              onClick={() => props.handlePageChange("Projects")}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className="text-light custom-nav"
+              onClick={() => props.handlePageChange("Contact")}
+            >
+              Contact
+            </Nav.Link>
+            <Nav.Link
+              href="#resume"
+              className="text-light custom-nav"
+              onClick={() => props.handlePageChange("Resume")}
+            >
+              Resume
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
