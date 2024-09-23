f1274965ff514297e5f1cfaae79688f6073154fb
linhklatt <112294288+linhklatt@users.noreply.github.com>
2024-09-22 21:43:55 -0700

    switched NavBar to bootstrap

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -2,68 +2,110 @@ import "./navbar.css";
 import logo from "../../assets/images/logo.png";
 import contactImg from "../../assets/images/contact.png";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <img src={logo} alt="Logo" className="logo" />
-      <div className="desktopMenu">
-        <Link
-          activeClass="active"
-          to="intro"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="desktopMenuListItem"
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        {/* Logo */}
+
+        <img src={logo} alt="Logo" className="logo" />
+
+        {/* Hamburger button for mobile view */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded={isMenuOpen ? "true" : "false"}
+          aria-label="Toggle navigation"
+          onClick={toggleMenu}
         >
-          Home
-        </Link>
-        <Link
-          activeClass="active"
-          to="skills"
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-          className="desktopMenuListItem"
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Centered Links */}
+        <div
+          className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}
+          id="navbarNav"
         >
-          About Me
-        </Link>
-        <Link
-          activeClass="active"
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-          className="desktopMenuListItem"
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                activeClass="active"
+                to="intro"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                activeClass="active"
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+              >
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+              >
+                Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={500}
+              >
+                Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Contact Button (aligned to the right) */}
+        <button
+          className="btn btn-primary contact-btn"
+          onClick={() => {
+            document
+              .getElementById("contact")
+              .scrollIntoView({ behavior: "smooth" });
+          }}
         >
-          Skills
-        </Link>
-        <Link
-          activeClass="active"
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-50}
-          duration={500}
-          className="desktopMenuListItem"
-        >
-          Portfolio
-        </Link>
+          <img src={contactImg} alt="Contact Me" className="desktopMenuImg" />
+          Contact Me
+        </button>
       </div>
-      <button
-        className="desktopMenuBtn"
-        onClick={() => {
-          document
-            .getElementById("contact")
-            .scrollIntoView({ behavior: "smooth" });
-        }}
-      >
-        <img src={contactImg} alt="ContactImage" className="desktopMenuImg" />
-        Contact Me
-      </button>
     </nav>
   );
 };
